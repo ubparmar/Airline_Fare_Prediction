532a2866db4d80468140c1a092fd4c85f2a33e00
ubparmar <58878498+ubparmar@users.noreply.github.com>
2024-08-16 15:07:34 -0400

    Uploading Dataset

--- Airlinepricepredictor/urls.py
+++ Airlinepricepredictor/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Pricepredictiormain.urls')),
-    path('dashboard/', include('admin_dashboard.urls')), 
+   
     
 ]
