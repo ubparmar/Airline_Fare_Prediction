7dff3de81648075b98a7642f30d5b3e89f7fc247
ubparmar <58878498+ubparmar@users.noreply.github.com>
2024-08-16 17:30:25 -0400

    Final

--- Airlinepricepredictor/settings.py
+++ Airlinepricepredictor/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+#STATIC_URL = "static/"
 #For Production server
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #For local testing
